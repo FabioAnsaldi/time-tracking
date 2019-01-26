ba9c10ca5c9f4250b0d0474a884fcfc1df8f4f4c
MacBook Pro <macbookpro@MBP-di-MacBook.station>
2019-01-26 16:49:32 +0100

    Added formatter date fo projects

--- src/components/widgets/addnew/index.jsx
+++ src/components/widgets/addnew/index.jsx
@@ -8,10 +8,14 @@ import axios from "axios";
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
 import Avatar from '@material-ui/core/Avatar';
 import WorkIcon from '@material-ui/icons/Work';
 import Button from '@material-ui/core/Button';
-import Input from '@material-ui/core/Input';
+import PlayArrow from '@material-ui/icons/PlayArrow';
+import Typography from '@material-ui/core/Typography';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
 import Paper from '@material-ui/core/Paper';
 import {withStyles} from '@material-ui/core/styles';
 import Grid from "@material-ui/core/Grid";
@@ -52,6 +56,27 @@ export class Addnew extends Component {
         this.props.dispatch(actions.setValue(e.target.value));
     }
 
+    getDifferenceTime(date1, date2) {
+
+        date1 = new Date(date1);
+        date2 = new Date(date2);
+
+        let res = Math.abs(date1 - date2) / 1000;
+        let days = Math.floor(res / 86400);
+        let hours = Math.floor(res / 3600) % 24;
+        let minutes = Math.floor(res / 60) % 60;
+        let seconds = res % 60;
+
+        return {days, hours, minutes, seconds}
+    }
+
+    dateFormatter(obj) {
+
+        let date = this.getDifferenceTime(obj.startTime, obj.time);
+
+        return `${date.days} Days, ${date.hours} hours, ${date.minutes} minutes, ${date.seconds} minutes`;
+    }
+
     newProject(e) {
 
         let {projects, value} = this.props.addnewState;
@@ -85,7 +110,17 @@ export class Addnew extends Component {
             return (
                 <ListItem key={i}>
                     <Avatar><WorkIcon/></Avatar>
-                    <ListItemText primary={obj.name} secondary={obj.created}/>
+                    <ListItemText primary={obj.name} secondary={
+                        <React.Fragment>
+                            <Typography component="span" color="textPrimary">
+                                {obj.created}
+                            </Typography>
+                            {this.dateFormatter(obj)}
+                        </React.Fragment>
+                    }/>
+                    <ListItemSecondaryAction>
+                        <IconButton aria-label="Record"> <PlayArrow/> </IconButton>
+                    </ListItemSecondaryAction>
                 </ListItem>
             )
         });
@@ -97,7 +132,8 @@ export class Addnew extends Component {
                     <form onSubmit={this.newProject}>
                         <Grid container spacing={24}>
                             <Grid item xs={2}>
-                                <Input value={this.props.addnewState.value} onChange={this.setNewValue}/>
+                                <TextField id="project-name" value={this.props.addnewState.value}
+                                           onChange={this.setNewValue}/>
                             </Grid>
                             <Grid item xs={1}>
                                 <Button variant="contained" type="submit" onClick={this.newProject}>
